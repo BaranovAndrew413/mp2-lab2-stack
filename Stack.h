e459d49d02f3eb62b90365d628ba6c7bbe939484
vasya <e.semenuta@yandex.ru>
2019-11-27 16:06:09 +0300

    sss

--- Stack.h
+++ Stack.h
@@ -6,7 +6,55 @@ class Stack
 	int size;
 	int num;
 public:
-	Stack();
+	Stack(int _size = 10){
+		if (_size == 0) throw _size;
+		size = _size;
+		arr = new T[size];
+		num = -1;
+	}
+	Stack(const Stack &st){
+		size = st.size;
+		arr = new T[size];
+		num = st.num;
+		fot(int i = 0; i <= num; i++)
+		{
+			arr[i] = st.arr[i];
+		}
+	}
+	void push(T el)
+	{
+		if (num == size - 1)
+			throw num;
+		arr[num + 1] = el;
+		num++;
+	}
 	~Stack();
+	T pop(){
+		if (num == -1)
+			throw num;
+		else { num--;
+		return arr[num + 1];
+		}
+	}
+	T top(){
+		if (num == -1)
+			throw num;
+		else
+		{
+			return arr[num];
+		}
+	}
+	bool IsEmpty() const{
+		if (num == -1)
+			return true;
+		else 
+			return false;
+	}
+	bool IsFull() const{
+		if (num == size - 1)
+			return true;
+		else 
+			return false;
+	}
 };
 
